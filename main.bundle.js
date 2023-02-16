7bfb79a57338ebdee7e0d18f571a6a814682c982
SirIsaacNeutron <dbadir@cox.net>
2023-02-15 22:47:28 -0800

    Add initial files

---
+++ main.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("webpack testing")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw4QiIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIndlYnBhY2sgdGVzdGluZ1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==

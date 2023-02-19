64acd356cf659111a76840714d52bb496081f6dc
SirIsaacNeutron <dbadir@cox.net>
2023-02-19 01:33:45 -0800

    Add todo creation

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,3 +1,5 @@
+import Todo from "./Todo"
+
 export default class TodoList {
     constructor(title) {
         this.title = title
@@ -23,13 +25,80 @@ export default class TodoList {
         const todos = document.createElement("div")
         todos.classList.add("todos")
 
-        for (const i = 0; i < this.list.length; ++i) {
-            // render todos
+        for (let i = 0; i < this.list.length; ++i) {
+            const todo = document.createElement("article")
+            todo.classList.add("todo")
+
+            const todoTitle = document.createElement("p")
+            todoTitle.textContent = this.list[i].title
+
+            todo.appendChild(todoTitle)
+
+            const todoDate = document.createElement("p")
+            todoDate.textContent = this.list[i].dueDate
+            todoDate.classList.add("date")
+
+            todo.appendChild(todoDate)
+
+            todos.appendChild(todo)
         }
 
         const addTodoBtn = document.createElement("button")
         addTodoBtn.classList.add("add-todo-btn")
         addTodoBtn.textContent = "+ Add todo"
+
+        addTodoBtn.addEventListener("click", () => {
+            const form = document.createElement("form")
+            form.setAttribute("method", "POST")
+            form.classList.add("add-todo-form")
+
+            const input = document.createElement("input")
+            input.setAttribute("type", "text")
+            input.setAttribute("name", "title")
+            input.setAttribute("required", "")
+            input.setAttribute("placeholder", "Todo title")
+
+            form.appendChild(input)
+
+            const formBtns = document.createElement("div")
+            formBtns.classList.add("form-btns")
+
+            const submitBtn = document.createElement("button")
+            submitBtn.textContent = "Add"
+            submitBtn.classList.add("form-btn")
+            submitBtn.classList.add("submit-btn")
+
+            formBtns.appendChild(submitBtn)
+            
+            const cancelBtn = document.createElement("button")
+            cancelBtn.textContent = "Cancel"
+            cancelBtn.classList.add("form-btn")
+            cancelBtn.classList.add("cancel-btn")
+
+            formBtns.appendChild(cancelBtn)
+
+            form.appendChild(formBtns)
+
+            cancelBtn.addEventListener("click", () => {
+                form.replaceWith(addTodoBtn)
+            })
+
+            form.addEventListener("submit", e => {
+                e.preventDefault()
+
+                console.log("Called")
+                const newTodoTitle = input.value
+
+                const newTodo = new Todo(newTodoTitle, "", "No date", "High")
+
+                this.addTodo(newTodo)
+
+                // Redraw the whole page
+                this.render()
+            })
+
+            addTodoBtn.replaceWith(form)
+        })
 
         todos.appendChild(addTodoBtn)
         main.appendChild(todos)

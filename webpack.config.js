fb3f3048b38ab48c88b35286eec5bae224c077ee
SirIsaacNeutron <dbadir@cox.net>
2023-02-23 22:40:04 -0800

    Add development mode hash

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "[name].bundle.js",
+        filename: "[name].bundle[hash].js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
-    //devtool: "inline-source-map",
+    devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
             title: "Todo List",
